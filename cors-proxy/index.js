578f18d39675dae8fc51086adf3ad330122816be
Emerson Vale <165671142+emersonsvale@users.noreply.github.com>
2025-07-21 15:43:44 -0300

    Update index.js

--- cors-proxy/index.js
+++ cors-proxy/index.js
@@ -4,24 +4,21 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.all("/", async (req, res) => {
     const url = req.query.url;
     if (!url) return res.status(400).send("Missing url query parameter");
 
     try {
-        const response = await axios({
-            method: req.method,
-            url,
-            headers: req.headers,
-            data: req.body,
-            responseType: "arraybuffer",
+        const response = await axios.get(url, {
+            responseType: "arraybuffer", // <- necessário para binários
         });
 
+        // Define o content-type da resposta original (por ex: image/jpeg)
         res.set("Content-Type", response.headers["content-type"]);
-        res.status(response.status).send(response.data);
+
+        // Retorna o corpo binário da imagem
+        res.end(response.data, "binary");
     } catch (error) {
         res.status(500).send("Error in proxying request: " + error.message);
     }
